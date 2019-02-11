6cfe85e418b4819695f07ed6b4d07c6b9db1992c
zhangxh <zhangxhbeta@gmail.com>
2019-02-11 16:58:50 +0800

    Set node use taobao mirros

--- install.sh
+++ install.sh
@@ -222,6 +222,19 @@ require_nvm stable
 # always pin versions (no surprises, consistent dev/build machines)
 npm config set save-exact true
 
+# set taobao mirrors
+npm set registry https://registry.npm.taobao.org
+npm set disturl https://npm.taobao.org/dist
+
+npm set sass_binary_site https://npm.taobao.org/mirrors/node-sass
+npm set electron_mirror https://npm.taobao.org/mirrors/electron
+npm set puppeteer_download_host https://npm.taobao.org/mirrors
+npm set chromedriver_cdnurl https://npm.taobao.org/mirrors/chromedriver
+npm set operadriver_cdnurl https://npm.taobao.org/mirrors/operadriver
+npm set phantomjs_cdnurl https://npm.taobao.org/mirrors/phantomjs
+npm set selenium_cdnurl https://npm.taobao.org/mirrors/selenium
+npm set node_inspector_cdnurl https://npm.taobao.org/mirrors/node-inspector
+
 #####################################
 # Now we can switch to node.js mode
 # for better maintainability and
